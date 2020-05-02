b725e3aee0323ee93beb8a01084d71bf8e564b14
mason roberts <mrobe18@gmail.com>
2020-05-01 22:55:25 -0400

    testing progress

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -27,7 +27,7 @@ router.post('/login', (req, res) => {
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user);
-        res.status(200).json({ msg: `${user.username} logged in` })
+        res.status(200).json({ msg: `${user.username} logged in`, token });
       }else{
         res.status(401).json({ msg: 'invalid credentials' });
       }
