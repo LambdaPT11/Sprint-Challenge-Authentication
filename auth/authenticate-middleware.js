d8ecccc9939a3356b0b011e57c14cab405823da2
mason roberts <mrobe18@gmail.com>
2020-05-01 22:16:55 -0400

    created user models and user router, worked on middleware, brought users into server. runs on 3300, not tested, surprised it ran.

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,19 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET || 'keep it secret'
 
 module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (suthorization) {
+    jwt.verify(authorization, secret, (err, decodedToken) => {
+      if(err) {
+        res.status(401).json({ msg: 'invalid token' })
+      }else{
+        req.token = decodedToken;
+        next();
+      }
+    })
+  }else{
   res.status(401).json({ you: 'shall not pass!' });
+  }
 };
